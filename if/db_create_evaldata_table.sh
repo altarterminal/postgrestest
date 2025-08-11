3416a8844c185f3e2513bae5a82458b947fbee3e
altarterminal <118420278+altarterminal@users.noreply.github.com>
2025-08-11 11:05:27 +0900

    add db_create_evaldata_table.sh

---
+++ if/db_create_evaldata_table.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -u
+
+#####################################################################
+# help
+#####################################################################
+
+print_usage_and_exit () {
+  cat <<-USAGE 1>&2
+Usage   : ${0##*/} <project name> <project version> <device name> <file>
+Options : -d
+
+Create an evaluation data table.
+
+-d: Enable dry-run (only judge whether you can create the table).
+USAGE
+  exit 1
+}
+
+#####################################################################
+# setting
+#####################################################################
+
+THIS_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+TOP_DIR=$(dirname "${THIS_DIR}")
+SCRIPT_DIR="${TOP_DIR}/sh/input_data"
+SCRIPT_FILE="${SCRIPT_DIR}/create_evaldata_table.sh"
+
+if [ ! -f "${SCRIPT_FILE}" ] || [ ! -x "${SCRIPT_FILE}" ]; then
+  echo "ERROR:${0##*/}: invalid script specified <${SCRIPT_FILE}>" 1>&2
+  exit 1
+fi
+
+#####################################################################
+# call
+#####################################################################
+
+"${SCRIPT_FILE}" "$@"
