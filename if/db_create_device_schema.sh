17b1cac04162dd4ae15fda303666a2e00f7e25f8
altarterminal <118420278+nm-jp@users.noreply.github.com>
2025-08-28 09:41:37 +0900

    add new if

---
+++ if/db_create_device_schema.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -u
+
+#####################################################################
+# setting
+#####################################################################
+
+THIS_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+SCRIPT_DIR="${THIS_DIR}/../sh/input_data"
+SCRIPT_FILE="${SCRIPT_DIR}/create_device_schema.sh"
+
+if [ ! -f "${SCRIPT_FILE}" ] || [ ! -x "${SCRIPT_FILE}" ]; then
+  echo "ERROR:${0##*/}: invalid script specified <${SCRIPT_FILE}>" 1>&2
+  exit 1
+fi
+
+#####################################################################
+# call
+#####################################################################
+
+"${SCRIPT_FILE}" "$@"
